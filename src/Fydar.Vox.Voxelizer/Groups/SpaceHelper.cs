5698420b72cf7cc1db655dceb45c083e8f440cde
Anthony Marmont <dev.anthonymarmont@gmail.com>
2020-08-15 01:14:58 +0100

    🐛 Fixed greedy mesh translation; it now greedily meshes a single-axis

---
+++ src/Fydar.Vox.Voxelizer/Groups/SpaceHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace Fydar.Vox.Voxelizer
+{
+	public static class SpaceHelper
+	{
+		public static void RotatePosition(Vector3SByte position, VoxelNormal normal, out Vector2SByte planePosition, out int depth)
+		{
+			var rotated = VoxelNormal.RotateAroundOrigin(position, normal);
+			planePosition = new Vector2SByte(rotated.x, rotated.y);
+			depth = rotated.z;
+		}
+	}
+}
