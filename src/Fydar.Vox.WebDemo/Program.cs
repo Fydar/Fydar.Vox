50fa0058571dd53635c2fba52495d652d2a87178
fydar <fydar.dev@gmail.com>
2020-11-16 19:33:11 +0000

    ✨ Improved CSS Voxel-Art renderer

--- src/Fydar.Vox.WebDemo/Program.cs
+++ src/Fydar.Vox.WebDemo/Program.cs
@@ -4,16 +4,16 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Fydar.Vox.Web
+namespace Fydar.Vox.WebDemo
 {
 	public class Program
 	{
 		public static async Task Main(string[] args)
 		{
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
-			builder.RootComponents.Add<App>("app");
+			builder.RootComponents.Add<App>("#app");
 
-			builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 			await builder.Build().RunAsync();
 		}
